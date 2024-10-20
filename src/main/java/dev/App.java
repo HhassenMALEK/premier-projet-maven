fe2871cbce02023dafd202cb511539b39cc6c721
Votre Nom <h.hassen.malek@gmail.com>
2024-10-20 17:39:48 +0200

    Ajout Figlet

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,13 +1,18 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+import java.io.IOException;
+
+
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException{
+
+        System.out.println( FigletFont.convertOneLine("Hassen MALEK") );
     }
 }
