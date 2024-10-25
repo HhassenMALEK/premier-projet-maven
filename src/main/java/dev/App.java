da9d57f6a04141a5273e4549e5cead1d39a66586
Votre Nom <h.hassen.malek@gmail.com>
2024-10-25 22:02:50 +0200

    tp02 fini

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -4,11 +4,6 @@ import com.github.lalyos.jfiglet.FigletFont;
 import java.io.IOException;
 
 
-
-/**
- * Hello world!
- *
- */
 public class App
 {
     public static void main( String[] args ) throws IOException{
